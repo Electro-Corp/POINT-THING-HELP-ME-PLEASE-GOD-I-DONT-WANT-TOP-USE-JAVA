90a5833a89ab50fa67fea64ac050669266a2bf65
Electro-Corp <chinmaytanmay@outlook.com>
2022-08-31 09:51:45 -0600

    Add files via upload

--- lineandpoint/Line.java
+++ lineandpoint/Line.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 public class Line{
     Point a;
     Point b;
+
     Line(Point a, Point b){
         this.a = a;
         this.b = b;
@@ -30,10 +31,51 @@ public class Line{
     }
     Line getPerpBisector(){
         Point d = new Point(this.getMidpoint().x,this.getMidpoint().y);
-        double disA 
-        Point g = new Point();
-        Line e = new Line(d,g);
+        //(xcos(θ)−ysin(θ),xsin(θ)+ycos(θ)).
         
+        
+        double negslope = this.getSlope();
+        negslope = negslope * -1;
+        
+        double anewx = a.x - d.x;
+        double anewy = a.y = d.y;
+        
+        double bnewx = b.x - d.x;
+        double bnewy = b.y - d.y;
+        
+        double gx = anewx * Math.cos(Math.toRadians(90));
+        gx = gx - (anewy * Math.sin(Math.toRadians(90)));
+        double gy = anewx * Math.sin(Math.toRadians(90));
+        gy = gy + (anewy * Math.cos(Math.toRadians(90)));
+        
+        gy = gy + d.y;
+        gx = gx + d.x;
+        
+        double fx = bnewx * Math.cos(Math.toRadians(90));
+        fx = fx - (bnewy * Math.sin(Math.toRadians(90)));
+        double fy = bnewx * Math.sin(Math.toRadians(90));
+        fy = fy + (bnewy * Math.cos(Math.toRadians(90)));
+        
+        fx = fx + d.x;
+        fy = fy + d.y;    
+    
+        
+        //Point g = new Point(((a.x*Math.cos(Math.toRadians(90)))-(a.y-Math.sin(Math.toRadians(90)))), a.y);
+        //Point f = new Point(((b.x*Math.sin(Math.toRadians(90)))-(b.y-Math.cos(Math.toRadians(90)))), b.y);
+        Point g = new Point(gx,gy);
+        Point f = new Point(fx,fy);
+        Line e = new Line(g,f);
+        
+        //double a_ok = a.y - b.y;
+        //double b_ok = a.x - b.x;
+        /*double disA = (d.x-a.x);
+        double disB = (d.x-b.x);
+    
+        
+        Point g = new Point(2*disA + d.x,a.y);
+        Point f = new Point(2*disB + d.x, b.y);
+        Line e = new Line(g,f);
+        */
         return e;
     }
 }
--- lineandpoint/Main.java
+++ lineandpoint/Main.java
@@ -2,11 +2,13 @@ public class Main{
     public static void Main(String[] args){
         GrapherPanel panel = new GrapherPanel();
         
-        Point a = new Point(3,4);
-        Point b = new Point(10,5);
+        Point a = new Point(6,3);
+        Point b = new Point(-4,-1);
         Line c = new Line(a,b);
         panel.plotLine(c);
+        System.out.println("hey");
         Line d = c.getPerpBisector();
+        System.out.println("done lamo");
         panel.plotLine(d);
     }
 }
